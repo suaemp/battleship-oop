7bc9a47e5ffecf228f6a4001acbd50d6387db2fc
suaemp <wiktoria.sladowska@gmail.com>
2022-03-17 19:07:59 +0100

    commit

--- src/com/codecool/battleship/Main.java
+++ src/com/codecool/battleship/Main.java
@@ -4,27 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(ShipType.values().length);
-
         Battleship battleship = new Battleship();
-
         battleship.menuInteraction();
-
-
-/*
-        how to get to enum types values:
-
-        SquareStatus[] shipSymbols = SquareStatus.values();
-        for (ShipType shipLength : shipsLengths) {
-            // use getter method to get the value
-            System.out.println("name: " + shipLength.name() +
-                    "; length: " + shipLength.getAction() +
-                    "; quantity: " + shipLength.getQuantity());
-        }
-
-        for (SquareStatus shipLength : shipSymbols) {
-            // use getter method to get the value
-            System.out.println("name: " + shipLength.name() +
-                    "; symbol: " + shipLength.getSymbol());}
-*/    }
+    }
 }
+// menu first, battleship later
